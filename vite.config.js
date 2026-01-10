78ed18662c653637ec99df1836b791d6d692ed59
Amrithchandh <amridchandhrd@gmail.com>
2026-01-10 10:00:14 +0530

    Optimize vite config for local serve vs production build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-﻿import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/nudge-/',
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/nudge-/',
   plugins: [react()],
-})
+}))

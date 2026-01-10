62f3cfdab3888733242b556dc4c98a062d29333d
Amrithchandh <amridchandhrd@gmail.com>
2026-01-10 10:56:59 +0530

    Update deployment base path for nudgebackup

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/nudge-/',
+  base: command === 'serve' ? '/' : '/nudgebackup/',
   plugins: [react()],
 }))
